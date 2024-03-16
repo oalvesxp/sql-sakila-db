3466baf6f2348e0cdc7c6097fc271ec03f87c534
Osvaldo Alves <oalves.py@gmail.com>
2024-03-16 14:23:26 -0300

    Adding SQL example for create a temp column and for command AS

---
+++ Sakila_Scripts/01-Entendo_SQL/04-Coluna_temporaria_e_AS.sql
@@ -0,0 +1,38 @@
+/* SQL Doumentation WHERE.sql
+    @Coluna Temp:   Em uma consulta podemos criar uma coluna temporária para armazenar dados de uma operação matematica (por exemplo).
+    @AS:            Comando de aliases, utilizado para renomear uma coluna para leitura (não altera a estrutura do banco).
+
+    @type SQL
+    @author Osvaldo A. Neto
+    @since 16/03/2024
+    @history @since, @author, @description
+*/
+
+-- Demanda: 
+-- A equipe de Marketing quer um relatório de compras
+-- da Cliente Mary Smith, e uma simulação de 10% de desconto
+-- para uma possível campanha de e-mail marketing
+
+USE sakila;
+
+-- Descobrindo o ID da Cliente Mary Smith
+SELECT customer_id
+    , first_name
+    , last_name
+FROM customer
+WHERE first_name = 'Mary'
+    AND last_name = 'Smith'
+
+-- Listando todas as compras desta cliente
+SELECT customer_id
+    , amount
+FROM payment
+WHERE customer_id = '1'
+
+-- Criando uma tabela temporária com o desconto de 10%
+-- e nomeando a coluna para '10% discount'
+SELECT customer_id
+    , amount
+    , amount - (amount * 0.10) AS '10% discount'
+FROM payment
+WHERE customer_id = '1'
