360cae60ae6232df596ea7a534126df2c3804349
Osvaldo Alves <oalves.py@gmail.com>
2024-03-17 12:27:22 -0300

    Adding SQL example for command BETWEEN

---
+++ Sakila_Scripts/02-Consultas_complexas/02-BETWEEN.sql
@@ -0,0 +1,14 @@
+/* SQL Doumentation IN.sql
+    @IN:           Comando para consultar um valor dentro de um RANGE expecífico.
+
+    @type SQL
+    @author Osvaldo A. Neto
+    @since 17/03/2024
+    @history @since, @author, @description
+*/
+
+USE sakila;
+
+SELECT *
+FROM payment
+WHERE amount BETWEEN '1.99' AND '3.99';
